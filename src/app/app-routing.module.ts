d17858fddf5aeebf04029c3931ece635276606c3
Paco <>
2019-04-11 15:04:14 +0200

    Update app-routing.module.ts
    Little changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SignupComponent} from './auth/signup/signup.component';
-import {WelcomeComponent} from './welcome/welcome.component';
 import {LoginComponent} from './auth/login/login.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ProductsListComponent} from './products-list/products-list.component';
+import {HomeComponent} from './core/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'signup', component: SignupComponent},
   { path: 'login', component: LoginComponent},
   { path: 'shopping-list', component: ShoppingListComponent},
